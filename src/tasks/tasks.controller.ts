2213617c6f83fae982e8dfa326c17578fb525518
Aleksandr Borkov <sbor767@yandex.ru>
2020-02-05 03:16:40 +0300

    Implement getAllTask methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 // Defines reaction for '/tasks' query path
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
+}
