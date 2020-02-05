eef4b227ad5c87c70e35804bc72bf2cb5ee2f0b0
Aleksandr Borkov <sbor767@yandex.ru>
2020-02-06 00:35:04 +0300

    createTask controller method and some typo fixes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
 // Defines reaction for '/tasks' query path
 @Controller('tasks')
 export class TasksController {
-  constructor(private taskService: TasksService) {}
+  constructor(private tasksService: TasksService) {}
 
   @Get()
   getAllTasks(): Task[] {
-    return this.taskService.getAllTasks();
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
   }
 }
