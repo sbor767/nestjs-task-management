1a50af14a031f1bc19045753ac24a0acf7f06a9a
Aleksandr Borkov <sbor767@yandex.ru>
2020-02-10 03:04:05 +0300

    4#48 getting tasks - refactored to extract logics to Repository // Some wierd test by Postman for complex conditions

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,29 +13,12 @@ export class TasksService {
     private taskRepository: TaskRepository,
   ) {}
 
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto);
+  }
 
-  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-
-  //   let tasks = this.getAllTasks();
-  //   if (status) {
-  //     tasks = tasks.filter(task => task.status === status);
-  //   }
-    
-  //   if (search) {
-  //     tasks = tasks.filter(task => 
-  //       task.title.includes(search) || task.description.includes(search)
-  //     );
-  //   }
-
-  //   return tasks;
-  // }
-
-    async getTaskById(id: number): Promise<Task> {
-      const found = await this.taskRepository.findOne(id);
+  async getTaskById(id: number): Promise<Task> {
+    const found = await this.taskRepository.findOne(id);
 
     if (!found) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
