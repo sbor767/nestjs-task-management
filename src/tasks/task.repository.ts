6704c3039af24b70a271a44a800d1854261c76f6
Aleksandr Borkov <sbor767@yandex.ru>
2020-02-11 23:02:53 +0300

    7#70 use error log through Logger

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -4,9 +4,12 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from "./task-status.enum";
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { User } from 'src/auth/user.entity';
+import { Logger, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+  private logger = new Logger('TaskRepository');
+
   async getTasks(
     filterDto: GetTasksFilterDto,
     user: User,
@@ -23,9 +26,14 @@ export class TaskRepository extends Repository<Task> {
     if (search) {
       query.andWhere('(task.title LIKE :search OR task.description LIKE :search)', { search: `%${search}%` });
     }
-
-    const tasks = await query.getMany();
-    return tasks;
+    
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch (error) {
+      this.logger.error(`Failed to get tasks for user "${user.username}", Filters: ${JSON.stringify(filterDto)}`, error.stack);
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(
@@ -38,7 +46,13 @@ export class TaskRepository extends Repository<Task> {
     task.description = description;
     task.status = TaskStatus.OPEN;
     task.user = user;
-    await task.save();
+
+    try {
+      await task.save();
+    } catch (error) {
+      this.logger.error(`Failed to create a task for user "${user.username}", Data: ${createTaskDto}`, error.stack);
+      throw new InternalServerErrorException();
+    }
 
     delete task.user; // because it contains sensitive info (!)
 
