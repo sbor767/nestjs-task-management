b7b457d26980e46649d033762194aa8abefe6c4b
Aleksandr Borkov <sbor767@yandex.ru>
2020-02-28 17:35:55 +0300

    Fix jwt.strategy file name

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './jwt-strategy';
+import { JwtStrategy } from './jwt.strategy';
 import * as config from 'config';
 
 const jwtConfig = config.get('jwt');
